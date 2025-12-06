04daebe0dd025a8271b23198dd3f199f0a625cec
pravinITOI <pravin@itoi.org>
2025-12-06 11:04:36 +0530

    icons integrated

---
+++ apps/frontend/src/components/atoms/ToolTip/Tooltip.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@components/ui/ToolTip';
+import { cn } from 'apps/frontend/lib';
+import { ReactElement, ReactNode } from 'react';
+
+export type ToolTipPosition = 'bottom' | 'left' | 'right' | 'top';
+export const ToolTip = ({
+  children,
+  tipMessage,
+  tipClassName,
+  side = 'bottom',
+  element,
+  ContentClassName,
+}: {
+  children: ReactElement;
+  tipMessage: ReactNode;
+  tipClassName?: string;
+  side?: ToolTipPosition;
+  element?: any;
+  ContentClassName?: string;
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        {tipMessage ? (
+          <TooltipContent side={side} className={ContentClassName}>
+            {element ? (
+              element
+            ) : (
+              <div className={cn('text-sm', tipClassName)}>{tipMessage}</div>
+            )}
+          </TooltipContent>
+        ) : null}
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
