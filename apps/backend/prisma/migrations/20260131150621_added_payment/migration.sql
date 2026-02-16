17a6628c81cb6e9aa931f73a2fab1a9a362f64f8
Shijin-ITOI <shijinkumar@itoi.org>
2026-02-16 09:50:10 +0530

    updated sql to potgresql (#19)
    * updated sql to potgresql
    
    * feat: Add Nx project configuration for the backend application and include Prisma generation in the postinstall script.

--- apps/backend/prisma/migrations/20260131150621_added_payment/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `payment_month` on the `payment` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `payment` DROP COLUMN `payment_month`;
